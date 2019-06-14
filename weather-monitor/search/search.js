7ecb067d46e47706c0b9cae1c54ac6541e515a0d
Yauheni Skibinski <stikler51@gmail.com>
2019-06-14 13:55:18 +0300

    feat: add bg to monitor section

--- weather-monitor/search/search.js
+++ weather-monitor/search/search.js
@@ -1,16 +1,60 @@
-const apiKey = 'a37fd266dcf0de1f7a2a058e96a7cabe';
+// const apiKey = 'a37fd266dcf0de1f7a2a058e96a7cabe';
 
-async function getResponse(q) {
-  const request = `https://api.openweathermap.org/data/2.5/weather?q=${q}&appid=${apiKey}&units=metric`;
-  const response = await fetch(request)
-    .then(resp => resp.json());
+const testResponse = {
+  coord: {
+    lon: 27.56,
+    lat: 53.9,
+  },
+  weather: [
+    {
+      id: 800,
+      main: 'Clear',
+      description: 'clear sky',
+      icon: '01d',
+    }],
+  base: 'stations',
+  main: {
+    temp: 30,
+    pressure: 1018,
+    humidity: 51,
+    temp_min: 30,
+    temp_max: 30,
+  },
+  visibility: 10000,
+  wind: {
+    speed: 4,
+    deg: 220,
+    gust: 7,
+  },
+  clouds: {
+    all: 0,
+  },
+  dt: 1560504912,
+  sys: {
+    type: 1,
+    id: 8939,
+    message: 0.0059,
+    country: 'BY',
+    sunrise: 1560476278,
+    sunset: 1560537723,
+  },
+  timezone: 10800,
+  id: 625144,
+  name: 'Minsk',
+  cod: 200,
+};
 
-  return response;
+async function getResponse() {
+  // const request = `https://api.openweathermap.org/data/2.5/weather?q=${q}&appid=${apiKey}&units=metric`;
+  // const response = await fetch(request)
+  //   .then(resp => resp.json());
+
+  return testResponse;
 }
 
 async function search() {
-  const q = document.body.querySelector('.search__field').value;
-  const response = await getResponse(q)
+  // const q = document.body.querySelector('.search__field').value;
+  const response = await getResponse()
     .then(data => data);
   console.log(response);
 }
